7841aa4606c29226a05579cd5cf4338c38d14afb
LGey-tech <l-glebedev@yandex.ru>
2025-01-16 16:23:42 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,11 +1,7 @@
 temp_created_date = input('Дата создания заметки? Формат:dd.mm.yyyy')
 
-temp_created_date_1 = temp_created_date[0:5]
-
-print(f"Дата создания:", temp_created_date_1)
+print(f"Дата создания:", temp_created_date[0:5])
 
 temp_issue_date = input('Дата истечения заметки (дедлайн)')
 
-temp_issue_date1 = temp_issue_date[0:5]
-
-print(f"Дата истечения:", temp_issue_date1)
+print(f"Дата истечения:", temp_issue_date[0:5])
